15ff2aa83fc243532302166eb551a779bceb01d3
selcukarz <ariozselcuk@gmail.com>
2022-09-30 16:36:26 +0300

    Initial Commit

---
+++ BirthdayNoteTaker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BirthdayNoteTaker
+//
+//  Created by Selçuk Arıöz on 30.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
